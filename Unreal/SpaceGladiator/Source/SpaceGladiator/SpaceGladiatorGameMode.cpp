2ab52c7e525e1675fab63d520041c2d11c6601d6
Aljoscha Schmidt <aschmidt@techfak.uni-bielefeld.de>
2015-12-07 14:41:08 +0100

    Initialized Unreal Project

---
+++ Unreal/SpaceGladiator/Source/SpaceGladiator/SpaceGladiatorGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SpaceGladiator.h"
+#include "SpaceGladiatorGameMode.h"
+
+
+
+
---
+++ Unreal/SpaceGladiator/Source/SpaceGladiator/SpaceGladiatorGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SpaceGladiatorGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEGLADIATOR_API ASpaceGladiatorGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
