0e5abc64cf4dbe2ab6d6656795e8b8a7768aa0db
Stephan Wenninger <swenninger@techfak.uni-bielefeld.de>
2016-07-21 21:36:22 +0200

    New Icons and Colors. Needs tuning.

--- Unreal/SpaceGladiator/Source/SpaceGladiator/ItemType.h
+++ Unreal/SpaceGladiator/Source/SpaceGladiator/ItemType.h
@@ -5,29 +5,38 @@ enum class EItemType : uint8
 {
 	ItemType_None				UMETA(DisplayName = "Not Set"),
 	ItemType_InvertControls		UMETA(DisplayName = "Invert Controls"),
+	ItemType_InvertCamera		UMETA(DisplayName = "Invert Camera"),
 	ItemType_Magnet			 	UMETA(DisplayName = "Magnet"),
-	ItemType_Detonator			UMETA(DisplayName = "Detonator"),
+	ItemType_ProjectileAbsorb	UMETA(DisplayName = "Absorb Projectiles"),
+	ItemType_Unstoppable		UMETA(DisplayName = "Unstoppable")
 };
 
 // This needs to be kept in sync with the ItemType Enum
 static FLinearColor ItemColors[] = {
-	{ 0.0f, 0.0f , 0.0f, 0.0f },    // None
-	{ 1.0f, 0.25f, 0.0f, 1.0f },    // Invert
-	{ 1.0f, 0.0f , 0.6f, 1.0f },    // Magnet 
-	{ 0.0f, 0.5f , 1.0f, 1.0f },    // Detonator
+	{ 0.00f, 0.00f, 0.00f, 0.0f },    // None
+	{ 0.66f, 1.00f, 0.00f, 1.0f },    // Controls
+	{ 1.00f, 0.66f, 0.00f, 1.0f },    // Camera
+	{ 1.00f, 0.00f, 0.66f, 1.0f },    // Magnet 
+	{ 0.00f, 0.66f, 1.00f, 1.0f },    // Absorb
+	{ 0.66f, 0.00f, 1.00f, 1.0f },    // Unstoppable
 };
 
 // This needs to be kept in sync with the ItemType Enum
 static TCHAR* ItemGlowTextures[] = {
 	TEXT("Texture2D'/Engine/EditorLandscapeResources/WhiteSquareTexture.WhiteSquareTexture'"),
-	TEXT("Texture2D'/Game/Materials/ItemMaterials/ItemBomb.ItemBomb'"),
+	TEXT("Texture2D'/Game/Materials/ItemMaterials/ItemInvertControls.ItemInvertControls'"),
+	TEXT("Texture2D'/Game/Materials/ItemMaterials/ItemInvertCamera.ItemInvertCamera'"),
 	TEXT("Texture2D'/Game/Materials/ItemMaterials/ItemMagnet.ItemMagnet'"),
+	TEXT("Texture2D'/Game/Materials/ItemMaterials/ItemBomb.ItemBomb'"),
 	TEXT("Texture2D'/Game/Materials/ItemMaterials/ItemDetonator.ItemDetonator'"),
 };
 
 static TCHAR* ItemIcons[] = {
 	TEXT("Texture2D'/Engine/EditorLandscapeResources/WhiteSquareTexture.WhiteSquareTexture'"),
-	TEXT("Texture2D'/Game/Items/bomb.bomb'"),
+	TEXT("Texture2D'/Game/Items/InvertCamera.InvertControls'"),
+	TEXT("Texture2D'/Game/Items/InvertCamera.InvertCamera'"),
 	TEXT("Texture2D'/Game/Items/Magnet.Magnet'"),
+	// TODO: Replace with new icons
+	TEXT("Texture2D'/Game/Items/bomb.bomb'"),
 	TEXT("Texture2D'/Game/Items/ClusterMine.ClusterMine'"),
 };
